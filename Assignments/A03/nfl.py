e96cfe5f7eb68827140f48c10f21ac5d11d44648
Rissa-CSS <ccallender1996@gmail.com>
2019-02-06 14:06:35 -0400

    new nfl.py

--- Assignments/A03/nfl.py
+++ Assignments/A03/nfl.py
@@ -7,8 +7,9 @@ from time import sleep
 from random import shuffle
 
 scraper = BeautifulScraper()
-"""
+
 f = open("nfl_data.json","w")
+"""
 with urllib.request.urlopen("http://www.nfl.com/liveupdate/game-center/2012020500/2012020500_gtd.json") as url:
         data = json.loads(url.read().decode())
         f.write(json.dumps(data))
@@ -17,25 +18,45 @@ f.close
 """
 f = open("nfl_data.json","w")
 gameids = []
-delays = [.01,.02,.03,.04,.05]
+delays = [1,2,.03,.04,.05]
 season = ["POST","REG"]
-years = [x for x in range(2009,2019)]
+years = [x for x in range(2009,2011)]
 weeks = [x for x in range(1,18)]
-for seas in season:    
-         for year in years:
-                
-                for week in weeks:
-                        if seas == "POST":
-                                week=""
-                        url = "http://www.nfl.com/schedules/%s/%s%s" % (year,seas,week)
+gamedata = {}
+for year in years:
+        urls =[]
+        gamedata[year] = {}
+        for sea in season:
+                if sea == "POST":
+                        url ="http://www.nfl.com/schedules/%s/%s%s" % (year,sea,week)
                         page = scraper.go(url)
                         divs = page.find_all('div',{"class":"schedules-list-content"})
                         shuffle(delays)
-                        #print(divs)
+                        
                         for div in divs:
-                                if div['data-gameid'] not in divs:
+                                gameids.append(div['data-gameid'])
+                                gamedata[year][sea][div['data-gameid']] = {}
+
+                else:
+                        for week in weeks:
+                                url = "http://www.nfl.com/schedules/%s/%s%s" % (year,sea,week)
+                                page = scraper.go(url)
+                                divs = page.find_all('div',{"class":"schedules-list-content"})
+                                shuffle(delays)
+                        
+                                for div in divs:
                                         gameids.append(div['data-gameid'])
+                                        gamedata[year][sea][div['data-gameid']] = {}
+
+"""
 for game in gameids:
+        gamedata[year][sea][game] = 
         urllib.request.urlretrieve("http://www.nfl.com/liveupdate/game-center/%s/%s_gtd.json"%(game,game),'nfl/'+game+'.json')
 
+sorted(gameids)
+print(gameids)
+print(len(gameids))
+
+"""
+f.write(json.dumps(gamedata))
 print(len(gameids))
