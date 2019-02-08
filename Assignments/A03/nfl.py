977e05155b273079617c517deda716dae34f2566
Rissa-CSS <ccallender1996@gmail.com>
2019-02-08 04:45:15 -0400

    full range

--- Assignments/A03/nfl.py
+++ Assignments/A03/nfl.py
@@ -20,8 +20,8 @@ f = open("nfl_data.json","w")
 gameids = []
 delays = [1,2,.03,.04,.05]
 season = ["POST","REG"]
-years = [x for x in range(2009,2011)]
-weeks = [x for x in range(1,18)]
+years = [x for x in range(2009,2019)]
+weeks = [x for x in range(1,17)]
 gamedata = {}
 for year in years:
         urls =[]
@@ -29,7 +29,7 @@ for year in years:
         for sea in season:
                 if sea == "POST":
                         gamedata[year][sea] = {}
-                        url ="http://www.nfl.com/schedules/%s/%s%s" % (year,sea,week)
+                        url ="http://www.nfl.com/schedules/%s/%s" % (year,sea)
                         page = scraper.go(url)
                         divs = page.find_all('div',{"class":"schedules-list-content"})
                         shuffle(delays)
@@ -39,6 +39,7 @@ for year in years:
                                 gamedata[year][sea][div['data-gameid']] = {}
 
                 else:
+                        gamedata[year][sea] = {}
                         for week in weeks:
                                 url = "http://www.nfl.com/schedules/%s/%s%s" % (year,sea,week)
                                 page = scraper.go(url)
@@ -51,7 +52,10 @@ for year in years:
 
 
 for game in gameids:
-        urllib.request.urlretrieve("http://www.nfl.com/liveupdate/game-center/%s/%s_gtd.json"%(game,game),'nfl/'+game+'.json')
-
+        url ="http://www.nfl.com/liveupdate/game-center/%s/%s_gtd.json"%(game,game)
+        urllib.request.urlretrieve(url,'nfl/'+game+'.json')
+        #page = scraper.go(url)
+        #divs = page.find_all('pre',)
+        #print(divs.text)
 
 f.write(json.dumps(gamedata))
