39de822a1e1477910594ba3caa2145ba5594b4b4
Rissa-CSS <ccallender1996@gmail.com>
2019-03-13 15:40:20 -0500

    added sys.argv

--- Assignments/A05/ascii_art.py
+++ Assignments/A05/ascii_art.py
@@ -24,8 +24,10 @@ def img_to_ascii(**kwargs):
   
     width = kwargs.get('width',200)
     height = kwargs.get('height',200)
+    FontName=kwargs.get('FontName')
+    FontSize=kwargs.get('FontSize')
     path = kwargs.get('path',None)
-    sveImg = kwargs.get()
+    SaveImage = kwargs.get('SaveImage')
     #Opens the image
     im = Image.open(path)
     #Resizes the Image
@@ -37,7 +39,7 @@ def img_to_ascii(**kwargs):
     #Creates a new blank image
     newImg = Image.new('RGBA',(w,h),(255,255,255,255))
     #Sets the font
-    fnt = ImageFont.truetype('Shailena.ttf')
+    fnt = ImageFont.truetype(FontName,FontSize)
     #Draws on new image
     drawOnMe = ImageDraw.Draw(newImg)
     imlist = list(im.getdata())
@@ -63,7 +65,7 @@ def img_to_ascii(**kwargs):
     #Shows and saves the new image
     
     newImg.show()
-    newImg.save('output1.png')
+    newImg.save(SaveImage)
 
     
     
@@ -85,9 +87,12 @@ def resize(img,width):
 if __name__=='__main__':
     #Gets the path of the new image
     #path = 'C:/Users/Chloe/Documents/Users/Clorissa/Computer Science/Software/4883-SWTools-Callender/Assignments/A05/me.jpg'
-    path = sys.argv[1]
-    SavedImage = sys.argv[2]
-    fontused = sys.argv[3]
+    if(len(sys.argv) ==5):
+        path = sys.argv[1]
+        SavedImage = sys.argv[2]
+        FontName =sys.argv[3]
+        FontSize = sys.argv[4]
+     else
 
      #Calls the function convert the image to ascii    
-    img_to_ascii(path=path,sveImg=SavedImage,fnt=fontused)
+    img_to_ascii(path=path,SavedImage=SavedImage,FontName=FontName, FontSize=FontSize)
